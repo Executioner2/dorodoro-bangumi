2369f1465e56e8beffba293cc5b3e828b3dfe958
Executioner2 <executioner00123@gmail.com>
2025-04-28 18:22:19 +0800

    [test] 移除测试代码

--- src/bt/tracker.rs
+++ src/bt/tracker.rs
@@ -479,9 +479,6 @@ impl Tracker {
         &self,
         delay: u64,
     ) -> DelayedTask<Pin<Box<dyn Future<Output = u64> + Send>>> {
-        let delay = delay + 99999999;
-        let cmd = DiscoverPeerAddr { peers: vec![SocketAddr::from_str("192.168.2.177:3115").unwrap()] }.into();
-        self.emitter.send(&self.gasket_transfer_id, cmd).await.unwrap();
         trace!("创建 tracker 扫描任务\t{}秒后执行", delay);
         let send_to_gasket: Sender<TransferPtr> = self.emitter.get(&self.gasket_transfer_id).unwrap();
         let task = Tracker::scan_tracker(
