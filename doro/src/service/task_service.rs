85fcf7e201710dcd1c12a8afd107dd245b24ad3b
Executioner2 <executioner00123@gmail.com>
2025-08-11 11:36:05 +0800

    [style] 调整 service 的代码结构，使其更加清晰易读
    下一步：
     1. 实现磁力链接下载
     2. 继续实现后续基本接口
     3. [低优先级] 处理直接使用 rc4 加密传输，性能损失严重的问题
     4. 下载量计数存在bug，需要修复

--- doro/src/service/task_service.rs
+++ doro/src/service/task_service.rs
@@ -14,17 +14,12 @@ use crate::task::content::DownloadContent;
 use crate::task_manager::TaskManager;
 use crate::torrent::{Parse, Torrent, TorrentArc};
 
-/// 解析种子链接
-pub fn parse_torrent_link(link: &str) -> Result<TorrentRet> {
-    let info_hash = if_else!(link.starts_with("magnet:?"), parse_magnet_link(link)?, link);
-    if !doro_util::hash::check_info_hash(info_hash) {
-        return Err(anyhow!("不正确的 info_hash"));
-    }
-    todo!("{info_hash}")
-}
+// ===========================================================================
+// 内部函数
+// ===========================================================================
 
 /// 从磁力链接中取出 info_hash
-pub fn parse_magnet_link(link: &str) -> Result<&str> {
+fn parse_magnet_link(link: &str) -> Result<&str> {
     let prefix = "urn:btih:";
     let mut idx = link.find(prefix).ok_or(anyhow!("磁力链接格式错误"))?;
     idx += prefix.len();
@@ -35,6 +30,43 @@ pub fn parse_magnet_link(link: &str) -> Result<&str> {
     }
 
     Ok(&link[idx..idx + SHA1_ENCODEN_LEN])
+}
+
+/// 从本地文件中加载种子
+fn load_torrent_from_local_file(file_path: &str) -> Result<TorrentArc> {
+    TorrentArc::parse_torrent(file_path)
+}
+
+/// 从磁力链接中加载种子
+fn load_torrent_from_magnet_uri(magnet_uri: &str) -> Result<TorrentArc> {
+    todo!("待实现: {}", magnet_uri)
+}
+
+/// 从 rss 订阅中加载种子
+async fn load_torrent_from_rss_feed(rss_id: u64, guid: &str, url: &str) -> Result<TorrentArc> {
+    let content = tokio::time::timeout(HTTP_REQUEST_TIMEOUT, reqwest::get(url))
+        .await??
+        .bytes()
+        .await?;
+    let res = TorrentArc::parse_torrent(content);
+    if res.is_ok() {
+        let mut conn = Context::global().get_conn().await?;
+        conn.mark_read(rss_id, guid)?;
+    }
+    res
+}
+
+// ===========================================================================
+// 对外开放的函数
+// ===========================================================================
+
+/// 解析种子链接
+pub fn parse_torrent_link(link: &str) -> Result<TorrentRet> {
+    let info_hash = if_else!(link.starts_with("magnet:?"), parse_magnet_link(link)?, link);
+    if !doro_util::hash::check_info_hash(info_hash) {
+        return Err(anyhow!("不正确的 info_hash"));
+    }
+    todo!("{info_hash}")
 }
 
 /// 解析种子文件
@@ -58,30 +90,6 @@ pub fn parse_torrent_file(file_path: &str) -> Result<TorrentRet> {
         comment,
         files,
     })
-}
-
-/// 从本地文件中加载种子
-pub fn load_torrent_from_local_file(file_path: &str) -> Result<TorrentArc> {
-    TorrentArc::parse_torrent(file_path)
-}
-
-/// 从磁力链接中加载种子
-pub fn load_torrent_from_magnet_uri(magnet_uri: &str) -> Result<TorrentArc> {
-    todo!("待实现: {}", magnet_uri)
-}
-
-/// 从 rss 订阅中加载种子
-pub async fn load_torrent_from_rss_feed(rss_id: u64, guid: &str, url: &str) -> Result<TorrentArc> {
-    let content = tokio::time::timeout(HTTP_REQUEST_TIMEOUT, reqwest::get(url))
-        .await??
-        .bytes()
-        .await?;
-    let res = TorrentArc::parse_torrent(content);
-    if res.is_ok() {
-        let mut conn = Context::global().get_conn().await?;
-        conn.mark_read(rss_id, guid)?;
-    }
-    res
 }
 
 /// 添加任务
